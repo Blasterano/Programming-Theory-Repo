549ad676f18f2950347d123a2f3036c8cecf7e90
Aryan saraswat <38466649+Blasterano@users.noreply.github.com>
2022-08-29 16:51:30 +0530

    Interpolation on click
    moved object to a specified point when clicked using lerp function

--- Assets/Scripts/Shape.cs
+++ Assets/Scripts/Shape.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class Shape : MonoBehaviour
 {
+    [SerializeField]
+    float duration = 2;
+
+    public Transform placeHolder;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +20,24 @@ public class Shape : MonoBehaviour
     void Update()
     {
         
+    }
+    private void OnMouseDown()
+    {
+        Debug.Log(name);
+        StartCoroutine(Displace(placeHolder.position, duration));
+    }
+
+    IEnumerator Displace(Vector3 targetPosition,float duration)
+    {
+        float time = 0;
+        Vector3 startPos=transform.position;
+
+        while (time < duration)
+        {
+            transform.position = Vector3.Lerp(startPos, targetPosition, time / duration);
+            time+=Time.deltaTime;
+            yield return null;
+        }
+        transform.position = targetPosition;
     }
 }
